9349c2b21b98eb46ea3016201a0eb90124ec1814
Diogo Dourado <41040569+dcdourado@users.noreply.github.com>
2023-02-22 20:26:35 -0300

    Compile warnings and updates (#1)

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Blogit.Mixfile do
     [
       app: :blogit,
       version: @version,
-      elixir: "~> 1.5",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       docs: [
@@ -28,17 +28,17 @@ defmodule Blogit.Mixfile do
   end
 
   def application do
-    [applications: [:logger, :yaml_elixir], mod: {Blogit, []}]
+    [extra_applications: [:logger]]
   end
 
   defp deps do
     [
-      {:git_cli, "~> 0.2"},
-      {:earmark, "~> 1.2"},
-      {:yaml_elixir, "~> 2.1"},
-      {:calendar, "~> 0.17.3"},
-      {:ex_doc, ">= 0.15.0", only: :dev},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:git_cli, "~> 0.3.0"},
+      {:earmark, "~> 1.4.34"},
+      {:yaml_elixir, "~> 2.9.0"},
+      {:calendar, "~> 1.0.0"},
+      {:ex_doc, ">= 0.29.1", only: :dev},
+      {:dialyxir, "~> 1.2.0", only: [:dev], runtime: false}
     ]
   end
 
